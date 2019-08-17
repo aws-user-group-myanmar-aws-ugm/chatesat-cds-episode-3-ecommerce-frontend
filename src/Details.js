350765a0fd767a7526c8b4c4809dc35e357dea37
Ye Thu <yethu.personal@gmail.com>
2019-08-17 18:43:24 +0630

    Refactored details

--- src/Details.js
+++ src/Details.js
@@ -20,44 +20,58 @@ export default class Details extends React.Component {
     super(props);
     this.state = {
       data: null,
-      item: null
+      error: false,
+      loading: true
     };
   }
 
   componentDidMount() {
-    fetch(`http://${api_host}/api/phones`)
+    const { slug } = this.props.match.params;
+
+    fetch(`http://${api_host}/api/phones/${slug}`)
       .then(data => data.json())
       .then(json =>
         this.setState(
           Object.assign(this.state, {
-            data: json
-          }),
-          () => {
-            let item = find(propEq("slug", this.props.match.params.slug))(
-              this.state.data
-            );
-            this.setState(
-              Object.assign(this.state, {
-                item: item
-              })
-            );
-          }
+            data: json,
+            loading: false
+          })
+        )
+      )
+      .catch(err =>
+        this.setState(
+          Object.assign(this.state, {
+            error: true,
+            loading: false
+          })
         )
       );
   }
 
   render() {
-    if (this.state.data === null || this.state.item === null)
-      return <div>loading...</div>;
+    if (this.state.loading)
+      return (
+        <Container>
+          <div>loading...</div>
+        </Container>
+      );
+
+    if (!this.state.loading && this.state.error) {
+      return (
+        <Container>
+          <div>Phone not found</div>
+        </Container>
+      );
+    }
 
     return (
       <Container>
-        <Header as="h1">SellPhones™</Header>
+        <Header as="h1" onClick={() => this.props.history.push('/')}>SellPhones™</Header>
 
         <Grid stackable columns={2}>
           <Grid.Column>
             <Image
-              src={this.state.item.image}
+              src={this.state.data.image}
               onError={i => (i.target.src = require("./404.png"))}
               style={{
                 width: "600px",
@@ -72,13 +86,13 @@ export default class Details extends React.Component {
           </Grid.Column>
           <Grid.Column>
             <Header as="h1">
-              {this.state.item.name}
-              <Header.Subheader>{this.state.item.make}</Header.Subheader>
+              {this.state.data.name}
+              <Header.Subheader>{this.state.data.make}</Header.Subheader>
             </Header>
             <Statistic.Group widths="two">
               <Statistic horizontal>
                 <Statistic.Value>
-                  {parseInt(this.state.item.price).toLocaleString()}
+                  {parseInt(this.state.data.price).toLocaleString()}
                 </Statistic.Value>
                 <Statistic.Label>MMK</Statistic.Label>
               </Statistic>
@@ -97,43 +111,43 @@ export default class Details extends React.Component {
               <Table.Body>
                 <Table.Row>
                   <Table.Cell>Released</Table.Cell>
-                  <Table.Cell>{this.state.item.details.released}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.released}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Display Size</Table.Cell>
-                  <Table.Cell>{this.state.item.dimension} inches</Table.Cell>
+                  <Table.Cell>{this.state.data.dimension} inches</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Memory</Table.Cell>
-                  <Table.Cell>{this.state.item.memory}</Table.Cell>
+                  <Table.Cell>{this.state.data.memory}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Chipset</Table.Cell>
-                  <Table.Cell>{this.state.item.details.chipset}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.chipset}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>CPU</Table.Cell>
-                  <Table.Cell>{this.state.item.details.cpu}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.cpu}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Front Camera</Table.Cell>
-                  <Table.Cell>{this.state.item.details.fcamera}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.fcamera}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Rear Camera</Table.Cell>
-                  <Table.Cell>{this.state.item.details.rcamera}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.rcamera}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Resolution</Table.Cell>
-                  <Table.Cell>{this.state.item.details.resolution}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.resolution}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>GPU</Table.Cell>
-                  <Table.Cell>{this.state.item.details.gpu}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.gpu}</Table.Cell>
                 </Table.Row>
                 <Table.Row>
                   <Table.Cell>Operating System</Table.Cell>
-                  <Table.Cell>{this.state.item.details.os}</Table.Cell>
+                  <Table.Cell>{this.state.data.details.os}</Table.Cell>
                 </Table.Row>
               </Table.Body>
             </Table>
